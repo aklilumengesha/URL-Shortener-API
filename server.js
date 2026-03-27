5c66cba48eecbafc7ae717a808cc88a42e4ea22c
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-03-27 09:43:15 +0300

    refactor: remove console.log from server.js

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const start = async () => {
   try {
     const port = process.env.PORT || 3000;
     const host = process.env.HOST || '0.0.0.0';
-    
     await app.listen({ port, host });
-    
-    console.log(`🚀 Server running on http://localhost:${port}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
